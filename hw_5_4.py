a4b80fa2b00ddcb13c363de7d26fd943e81dc36c
Veronika Yeremenko <ver.sab1eremenk0@gmail.com>
2025-02-11 20:10:58 +0200

    task 4
    console bot helper with add error handling using decorators

---
+++ hw_5_4.py
@@ -0,0 +1,82 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Enter the argument for the command."
+        except Exception as e:
+            return f"An error occurred: {str(e)}"
+
+    return inner
+
+
+def parse_input(user_input):
+    """Розбирає введену команду та її аргументи."""
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+@input_error
+def add_contact(args, contacts):
+    """Додає новий контакт."""
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+@input_error
+def change_contact(args, contacts):
+    """Змінює номер телефону для існуючого контакту."""
+    name, phone = args
+    contacts[name] = phone
+    return "Contact updated."
+
+
+@input_error
+def show_phone(args, contacts):
+    """Показує номер телефону для заданого контакту."""
+    name = args[0]
+    if name in contacts:
+        return f"The phone number for {name} is {contacts[name]}."
+
+
+@input_error
+def show_all(contacts):
+    """Виводить всі збережені контакти та їхні номери."""
+    if not contacts:
+        return "No contacts saved."
+    result = "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    return result
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
