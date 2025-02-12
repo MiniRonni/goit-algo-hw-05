a1450bf79b9cc72a25e7e4311d7949bf8e2a9629
Veronika Yeremenko <ver.sab1eremenk0@gmail.com>
2025-02-12 14:53:51 +0200

    task 2

--- hw_5_2.py
+++ hw_5_2.py
@@ -2,25 +2,29 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    """Генератор для знаходження всіх дійсних чисел у тексті.
-    Використовуємо регулярний вираз для пошуку дійсних чисел
-    - \d+ - шукаємо числа
-    - \.?\d+ - шукаємо десяткові числа з можливим дробом"""
-    pattern = r'\b\d+(\.\d+)?\b'
+    """
+    Generator for finding all real numbers in a text.
+    Using a regular expression to find real numbers
+    - \d+ - search for integers;
+    - \.? - search for decimal point;
+    - \d+ - search for decimal part of number.
+    """
+    pattern = r'\b\d+(\.\d+)?\b' # Regular expression for numbers, including decimals
     
-    # Шукаємо всі числа і повертаємо їх по одному через yield
+    # Search for all numbers and return them one by one via yield
     for match in re.finditer(pattern, text):
-        yield float(match.group())
+        yield float(match.group()) # Convert the found number to float
 
 def sum_profit(text: str, func: Callable):
-    """Функція для обчислення загальної суми чисел, що були знайдені в тексті."""
-    # Викликаємо generator_numbers для отримання чисел
+    """
+    Function to calculate the total sum of numbers found in the text.
+    Uses a generator function to sum all numbers in the text.
+    """
     return sum(func(text))
 
-# Приклад використання:
+# Test case
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід,\
         доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
-# Обчислюємо суму
 total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+print(f"Total income: {total_income}")
